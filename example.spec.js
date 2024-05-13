97a40138f31c0782ff893412f335dd63b30643c9
Armin Højberg Ketirani <146706832+arke-nnit@users.noreply.github.com>
2024-05-13 20:24:39 +0200

    test 2

--- example.spec.js
+++ example.spec.js
@@ -16,4 +16,6 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+  // Test
 });
